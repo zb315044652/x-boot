3db02a0743c6513aee2cca44e5c426f4dfa821e4
zhangbin <zhangbin@uni-ubi.com>
2019-06-28 14:49:41 +0800

    删除定时任务

--- src/main/java/cn/exrick/xboot/XbootApplication.java
+++ src/main/java/cn/exrick/xboot/XbootApplication.java
@@ -2,7 +2,6 @@ package cn.exrick.xboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,8 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 //启用JPA审计
 @EnableJpaAuditing
-//启用缓存
-@EnableCaching
 //启用异步
 @EnableAsync
 //启用自带定时任务
